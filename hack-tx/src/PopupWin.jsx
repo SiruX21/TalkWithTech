0613b57d64deaf1612221eb63dc34b9adc1c6eee
Syrus Von Alexander Tolentino <36023438+SiruX21@users.noreply.github.com>
2024-11-03 08:39:40 -0600

    so it's bigger

--- hack-tx/src/PopupWin.jsx
+++ hack-tx/src/PopupWin.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import './PopupWin.css';
+
 const PopupWin = () => {
   return (
     <div className="popup-overlay" role="dialog" aria-modal="true">
+      <div className="popup-content">
+        <h1>You Won!!!</h1>
+      </div>
     </div>
   );
 };
