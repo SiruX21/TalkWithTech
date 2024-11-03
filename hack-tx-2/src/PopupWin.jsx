c643ad16a8379a56551c6f73e20f8324191a02bb
Syrus Von Alexander Tolentino <36023438+SiruX21@users.noreply.github.com>
2024-11-03 09:43:56 -0600

    popupwinn try to not die challenge

--- hack-tx-2/src/PopupWin.jsx
+++ hack-tx-2/src/PopupWin.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
-import './PopupWin.css';
-import Confetti from 'react-confetti-boom';
+import './PopupWin.css'; // Ensure this path is correct
 
 const PopupWin = () => {
   return (
     <div className="custom-popup-overlay" role="dialog" aria-modal="true">
       <div className="custom-popup-content">
         <h1 className="custom-popup-heading">You Won!!!</h1>
-        <Confetti mode="boom" particleCount={50} colors={['#ff577f', '#ff884b']} />
       </div>
     </div>
   );
