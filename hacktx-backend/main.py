7c723f16f7c7de00079001ff249df4628a943877
Syrus Von Alexander Tolentino <36023438+SiruX21@users.noreply.github.com>
2024-11-03 02:06:28 -0600

    Open up port to anyone in the IP Range

--- hacktx-backend/main.py
+++ hacktx-backend/main.py
@@ -62,4 +62,4 @@ def status():
 if __name__ == '__main__':
     with open("chat_history.txt", "w") as file:
         file.write("")
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
