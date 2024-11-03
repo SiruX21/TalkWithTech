c83b75bd659c3ed5d05d1587655e1650635ea5e9
Waz9pluz10 <139528953+dahrail@users.noreply.github.com>
2024-11-03 06:41:02 -0600

    Back to Colored Backgrounds

--- hacktx-start/src/App.jsx
+++ hacktx-start/src/App.jsx
@@ -1,51 +1,9 @@
-// import React, { useState, useEffect } from 'react';
-// import './App.css';
-
-// function App() {
-//   const games = ["Customer Simulator", "Teacher Simulator", "Interview Simulator"];
-//   const websites = ["https://customer.talkwith.tech", "https://www.yahoo.com", "https://www.bing.com"];
-//   const [currentGameIndex, setCurrentGameIndex] = useState(0);
-
-//   const changeGame = (direction) => {
-//     if (direction === 'left') {
-//       setCurrentGameIndex((prevIndex) => (prevIndex - 1 + games.length) % games.length);
-//     } else {
-//       setCurrentGameIndex((prevIndex) => (prevIndex + 1) % games.length);
-//     }
-//   };
-
-//   useEffect(() => {
-//     const colors = ['#a577b5', '#C65D3B', '#800000']; // Red, Burnt Orange, Maroon
-//     document.body.style.backgroundColor = colors[currentGameIndex];
-//   }, [currentGameIndex]);
-
-//   const handleSelectGame = () => {
-//     window.location.href = websites[currentGameIndex];
-//   };
-
-//   return (
-//     <div id="root">
-//       <div className="arrow-box left" onClick={() => changeGame('left')}>
-//         &#8592;
-//       </div>
-//       <div className="arrow-box right" onClick={() => changeGame('right')}>
-//         &#8594;
-//       </div>
-//       <div className="title-screen">{games[currentGameIndex]}</div>
-//       <button className="select-game-button" onClick={handleSelectGame}>Select Game</button>
-//     </div>
-//   );
-// }
-
-// export default App;
-
 import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
   const games = ["Customer Simulator", "Teacher Simulator", "Interview Simulator"];
   const websites = ["https://customer.talkwith.tech", "https://www.yahoo.com", "https://www.bing.com"];
-  const backgrounds = ["supermarket.png", "classroom.png", "interview.png"];
   const [currentGameIndex, setCurrentGameIndex] = useState(0);
 
   const changeGame = (direction) => {
@@ -57,7 +15,8 @@ function App() {
   };
 
   useEffect(() => {
-    document.body.style.backgroundImage = `url(${backgrounds[currentGameIndex]})`;
+    const colors = ['#a577b5', '#C65D3B', '#800000']; // Red, Burnt Orange, Maroon
+    document.body.style.backgroundColor = colors[currentGameIndex];
   }, [currentGameIndex]);
 
   const handleSelectGame = () => {
@@ -65,19 +24,18 @@ function App() {
   };
 
   return (
-    <>
+    <div id="root">
       <div className="arrow-box left" onClick={() => changeGame('left')}>
         &#8592;
       </div>
       <div className="arrow-box right" onClick={() => changeGame('right')}>
         &#8594;
       </div>
-      <div className="content">
-        <div className="title-screen">{games[currentGameIndex]}</div>
-        <button className="select-game-button" onClick={handleSelectGame}>Select Game</button>
-      </div>
-    </>
+      <div className="title-screen">{games[currentGameIndex]}</div>
+      <button className="select-game-button" onClick={handleSelectGame}>Select Game</button>
+    </div>
   );
 }
 
 export default App;
+
