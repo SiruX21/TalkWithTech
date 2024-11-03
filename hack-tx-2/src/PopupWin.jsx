909a4d127932ca8ae4913de443739d7a84bfba4b
Syrus Von Alexander Tolentino <36023438+SiruX21@users.noreply.github.com>
2024-11-03 09:35:04 -0600

    confetti time???

--- hack-tx-2/src/PopupWin.jsx
+++ hack-tx-2/src/PopupWin.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
-import './PopupWin.css'; // Ensure this path is correct
+import './PopupWin.css'; 
+import Confetti from 'react-confetti-boom';
+function MyApp() {
+  return <Confetti />;
+}
 
 const PopupWin = () => {
   return (
+    
     <div className="custom-popup-overlay" role="dialog" aria-modal="true">
       <div className="custom-popup-content">
         <h1 className="custom-popup-heading">You Won!!!</h1>
+        <Confetti mode="boom" particleCount={50} colors={['#ff577f', '#ff884b']} />
       </div>
     </div>
   );
