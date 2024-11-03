231596fcaebc07130af483f5073a8b212b9cecc8
Syrus Von Alexander Tolentino <36023438+SiruX21@users.noreply.github.com>
2024-11-03 09:40:36 -0600

    idk what this changes

--- hack-tx-2/src/PopupWin.jsx
+++ hack-tx-2/src/PopupWin.jsx
@@ -1,13 +1,9 @@
 import React from 'react';
-import './PopupWin.css'; 
+import './PopupWin.css';
 import Confetti from 'react-confetti-boom';
-function MyApp() {
-  return <Confetti />;
-}
 
 const PopupWin = () => {
   return (
-    
     <div className="custom-popup-overlay" role="dialog" aria-modal="true">
       <div className="custom-popup-content">
         <h1 className="custom-popup-heading">You Won!!!</h1>
