e9b2ebf410f9492e3f53f6ebc432ba0e0732e8e3
Syrus Von Alexander Tolentino <36023438+SiruX21@users.noreply.github.com>
2024-11-03 09:48:15 -0600

    bigger??

--- hack-tx/src/PopupWin.jsx
+++ hack-tx/src/PopupWin.jsx
@@ -7,7 +7,13 @@ const PopupWin = () => {
     <div className="custom-popup-overlay" role="dialog" aria-modal="true">
       <div className="custom-popup-content">
         <h1 className="custom-popup-heading">You Won!!!</h1>
-        <Confetti mode="boom" particleCount={5000} colors={['#ff577f', '#ff884b']} />
+        <Confetti 
+          mode="boom" 
+          particleCount={5000} 
+          colors={['#ff577f', '#ff884b']} 
+          spreadDeg={90} // Increase the spread angle
+          shapeSize={20} // Increase the size of the confetti particles
+        />
       </div>
     </div>
   );
